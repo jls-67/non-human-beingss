15c4babc0b50ebb4d948f26068426e44a56c7388
jls-67 <93082495+jls-67@users.noreply.github.com>
2021-12-11 20:59:04 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    let navBar = document.querySelector('.nav-list');
+    if (navBar.style.display === "block") {
+        navBar.style.display = "none";
+      } else {
+        navBar.style.display = "block";
+      }
+    }
+
+
